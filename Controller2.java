50673beba5b3977840dbcde01d2788b83123a622
LEVilleza <villeza.lee.euan@gmail.com>
2026-02-24 00:19:41 +0400

    Fixed Issue #1

--- Controller2.java
+++ Controller2.java
@@ -1,4 +1,4 @@
-package mvcexample1;
+package mvcexample;
 // Controller 2: displays views 3 and 4
 //               (displaying A and B components of the model)
 // Offers two button: clear views and refresh views - see below.
