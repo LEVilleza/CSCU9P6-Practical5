9a48aea09848f5ccc4d19c0e58d2d884cfc1a5c4
LEVilleza <villeza.lee.euan@gmail.com>
2026-02-24 00:11:29 +0400

    Fixed Issue #2

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String args[]) {
 
         String name = "Controller";
+        
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model, name); // Create first controller
         Controller2 c2 = new Controller2(model, name); // Create second controller
